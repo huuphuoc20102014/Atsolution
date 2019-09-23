bd9e0e66f2de73c8914ded08950b52af6ee0f866
Phước <phuocth97@gmail.com>
2019-09-23 10:50:23 +0700

    up

---
+++ AT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
