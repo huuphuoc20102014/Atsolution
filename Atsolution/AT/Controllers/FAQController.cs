5e5f60486893c5b5922282df1f6715a0db9de28e
Truong <truongpq197@gmail.com>
2019-09-24 17:02:12 +0700

    FAQ

---
+++ Atsolution/AT/Controllers/FAQController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AT.Efs;
+using AT.Efs.Entities;
+using AT.Models;
+
+namespace AT.Controllers
+{
+    public class FAQController : GsControllerBase<FAQController>
+    {
+        public FAQController(WebAtSolutionContext webContext) : base(webContext)
+        {
+
+        }
+        public IActionResult Index()
+        {
+            FAQViewModel model = new FAQViewModel();
+            model.faqList = _webContext.Faq.ToList();
+            return View(model);
+        }
+    }
+}
