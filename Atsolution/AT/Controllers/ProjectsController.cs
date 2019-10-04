7576fd75d194f5c05c96808862c49e5ecd133bba
Nghia <hoangxuannghia1999@gmail.com>
2019-10-04 08:38:58 +0700

    up du an

---
+++ Atsolution/AT/Controllers/ProjectsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AT.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        [HttpGet("du-an")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
