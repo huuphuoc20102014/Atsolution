3b21db223619b942af5f16ac846a5def0a992e58
Hiếu <truongvanhieu20041998@gmail.com>
2019-10-08 11:58:38 +0700

    update tin tuc

--- Atsolution/AT/Controllers/BlogTypeController.cs
+++ Atsolution/AT/Controllers/BlogTypeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AT.Efs.Entities;
+using AT.Helpers;
 using AT.Models;
 using Microsoft.AspNetCore.Mvc;
 
---
+++ Atsolution/AT/Helpers/BlogHelper.cs
@@ -0,0 +1,26 @@
+﻿using AT.Efs.Entities;
+using AT.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AT.Helpers
+{
+    public class BlogHelper
+    {
+        public static async Task<HomeViewModel> GetDataBlog (WebAtSolutionContext webContext)
+        {
+            HomeViewModel model = new HomeViewModel();
+            model.listBlog = webContext.News.ToList();
+            model.listBlogT = webContext.NewsType.ToList();
+            return model;
+        }
+
+        //public static async Task<List<Menu>> GetDataMenu(WebAtSolutionContext webContext)
+        //{
+        //    return await webContext.Menu.ToListAsync();
+        //}
+    }
+}
