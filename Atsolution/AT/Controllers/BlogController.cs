54f444d04976a10c6101769e80012b8a30f25534
Hiếu <truongvanhieu20041998@gmail.com>
2019-10-08 08:57:08 +0700

    phan trang

--- Atsolution/AT/Controllers/BlogController.cs
+++ Atsolution/AT/Controllers/BlogController.cs
@@ -2,16 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AT.Efs.Entities;
 using Microsoft.AspNetCore.Mvc;
+using PagedList;
+using System.Web;
+using AT.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AT.Controllers
 {
     public class BlogController : Controller
     {
+        WebAtSolutionContext _context = new WebAtSolutionContext();
         [HttpGet("tin-tuc")]
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int? pageNumber)
         {
-            return View();
+            if (pageNumber == null)
+            {
+                pageNumber = 1;
+            }
+
+            var students = from s in _context.News
+                           select s;
+
+            int pageSize = 3;
+            return View(await PaginatedList<News>.CreateAsync(students.AsNoTracking(), pageNumber ?? 1, pageSize));
         }
     }
 }
--- Atsolution/AT/Models/NewsViewModel.cs
+++ Atsolution/AT/Models/NewsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AT.Efs.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -25,5 +26,7 @@ namespace AT.Models
         public byte[] RowVersion { get; set; }
         public int RowStatus { get; set; }
         public string ImageSlug { get; set; }
+
+        public virtual NewsType FkNewsType { get; set; }
     }
 }
