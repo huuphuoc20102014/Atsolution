8a1041f9c041c70025d8956b3822a1d26c734690
Hiếu <truongvanhieu20041998@gmail.com>
2019-11-01 17:12:49 +0700

    AdminATSolution mới

---
+++ Atsolution/WebAdmin/ATAdmin/ATAdmin/Efs/Entities/ViewUserRole.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ATAdmin.Efs.Entities
+{
+    public partial class ViewUserRole : AtBaseECommerceEntity
+    {
+        public string TenQuyen { get; set; }
+        public string TenNguoiDung { get; set; }
+    }
+}
